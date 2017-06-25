692292a6021ce8c4b07a6e0f0301d23b28e40f98
Sergey Litvinov <slitvinov@gmail.com>
2017-06-25 23:17:23 +0200

    upd back

--- v/back/bootstrap.sh
+++ v/back/bootstrap.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
+
+avel() { # average ellipsoid
+    bop.avel $g.vtk ~/s/sh_$g/ply/rbcs-01*.ply
+}
+
+png() {
+    sh v.sh $g
+    py/m.py s/s.session $g.png
+}
+
+s=775x216+201+587
 t=/tmp/boot.$$
 trap 'rm -f $t' 1 2 3 15
-s=304x900+346+24
+crop() (
+    f=$g.png
+    convert -crop $s $f $t && mv $t $f
+)
 
-#l="1.0 2.0 3.0 4.0 5.0 6.0 7.0 8.0 9.0 10.0 11.0 12.0"
-#for g in $l; do sh v.sh $g; py/m.py s/s.session  $g.png; done
+l="1.0 2.0 3.0 6.0 10.0 12.0"
 
-for f in *.png
-do convert -crop $s $f $t && mv $t $f
-done
+for g in $l; do avel; done
+for g in $l; do png; done
+for g in $l; do crop; done
