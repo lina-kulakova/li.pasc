3d8842be381b279d9bf29d0c4848a4087df1131c
Sergey Litvinov <slitvinov@gmail.com>
2017-06-26 11:42:23 +0200

    upd

--- v/ks/bootstrap.sh
+++ v/ks/bootstrap.sh
@@ -4,20 +4,5 @@ avel() { # average ellipsoid
     bop.avel.ks $g.vtk ~/s/sh_$g/ply/rbcs-01*.ply
 }
 
-png() {
-    sh v.sh $g
-    py/m.py s/s.session $g.png
-}
-
-s=606x847+187+34
-t=/tmp/boot.$$
-trap 'rm -f $t' 1 2 3 15
-crop() (
-    f=$g.png
-    convert -crop $s $f $t && mv $t $f
-)
-
 l="1.0 2.0 3.0 6.0 10.0 12.0"
-#for g in $l; do avel; done
-#for g in $l; do png; done
-for g in $l; do crop; done
+for g in $l; do o=`avel`; echo $g $o; done | tee gfa.dat  # gdot freq a(X) a(Y)
--- v/ks/v.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-g=$1; vi.ln l l/e.visit $g.vtk
-
